b70f7a34d4254099ce6b6cc1f0abf8f2c29b5476
Kevin <kevin@nedellec.eu>
2023-03-03 23:21:56 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <Head>
+    <title>Frontend Mentor | Dictionary web app</title>
+    <meta name="description" content="Frontend Mentor | Dictionary web app" />
+    <meta name="keywords" content="dictionary" />
+    <meta name="author" content="kevinndlc" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/favicon-32x32.png"
+    />
+  </Head>
+
+  <div class="font-inter">
+    Dictionary web app
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      red: '#FF5252',
+      purple: 'A445ED',
+      white: '#FFFFFF',
+      gray: {
+        100: '#F4F4F4',
+        300: '#E9E9E9',
+        500: '#757575',
+        700: '#3A3A3A',
+        800: '#2D2D2D',
+        900: '#1F1F1F',
+        1000: '#050505',
+      },
+    },
+    fontFamily: {
+      inconsolata: ['Inconsolata', 'monospace'],
+      inter: ['Inter', 'sans-serif'],
+      lora: ['Lora', 'serif'],
+    },
+  },
+  plugins: [],
+};
